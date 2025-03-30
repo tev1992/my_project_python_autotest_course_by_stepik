fccfda041640ee7256aee572f6e512600d8113ac
Trukhin_Evgeniy <tev1992@yandex.ru>
2025-03-30 12:13:15 +0500

    добавление комментариев

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,9 +2,9 @@ from conftest import browser
 
 
 class BasePage():
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
+    def __init__(self, browser, url): # конструктор - метод который вызывается при создании обьекта
+        self.browser = browser # атрибуты класса
+        self.url = url # атрибуты класса
 
-    def open(self):
+    def open(self): # метод открытия страница
         self.browser.get(self.url)
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,9 @@
+# главная страница интернет магазина
+
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 
-class MainPage(BasePage):
+class MainPage(BasePage): # создается класс, который является наследником класса BasePage, чтобы имел доступ ко всем атрибутам и методам класса предка
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link") # self чтобы иметь доступ к атрибутам и методам класса
         login_link.click()
