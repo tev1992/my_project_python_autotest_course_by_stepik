46334c47223d3a9a8dbc9754e4afc25c184a7a90
Trukhin_Evgeniy <tev1992@yandex.ru>
2025-03-30 13:32:47 +0500

    добавление проверки элемента на странице

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,23 @@
-from conftest import browser
+# from conftest import browser
+from asyncio import timeout
 
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
-    def __init__(self, browser, url): # конструктор - метод который вызывается при создании обьекта
+    def __init__(self, browser, url, timeout=5): # конструктор - метод который вызывается при создании обьекта
         self.browser = browser # атрибуты класса
         self.url = url # атрибуты класса
+        self.browser.implicitly_wait(timeout)
 
     def open(self): # метод открытия страница
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,12 @@
 # главная страница интернет магазина
-
-from .base_page import BasePage
+from conftest import browser
+from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage): # создается класс, который является наследником класса BasePage, чтобы имел доступ ко всем атрибутам и методам класса предка
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link") # self чтобы иметь доступ к атрибутам и методам класса
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), 'Ссылка на логин не отображается'
