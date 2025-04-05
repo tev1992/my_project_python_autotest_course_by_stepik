7647e1a783124f3caa8709dd15a1d9846f0a24bd
Trukhin_Evgeniy <tev1992@yandex.ru>
2025-04-05 22:26:02 +0500

    Правки названия методов

--- pages/product_page.py
+++ pages/product_page.py
@@ -21,23 +21,23 @@ class ProductPage(BasePage):
         button_add_to_basket = self.browser.find_element(*ProductPageLocators.BOTTON_ADD_TO_BASKET)
         button_add_to_basket.click()
 
-    def get_price_product(self):
+    def get_price_product_text(self):
         price_element_text = self.browser.find_element(*ProductPageLocators.GET_PRODUCT_PRICE).text
         print(price_element_text)
         return price_element_text
 
-    def get_product_name(self):
+    def get_product_name_text(self):
         product_name = self.browser.find_element(*ProductPageLocators.GET_PRODUCT_NAME).text
         print(product_name)
         return product_name
 
     def message_product_added_to_basket(self): #language
-        expected_messege = f"{self.get_product_name()} {self.PRODUCT_ADDED_MESSAGES['ru']}"
+        expected_messege = f"{self.get_product_name_text()} {self.PRODUCT_ADDED_MESSAGES['ru']}"
         actual_messege = self.browser.find_element(*ProductPageLocators.MESSEGE_PRODUCT_ADDED).text #(self.PRODUCT_ADDED_MESSAGES.get(language, self.PRODUCT_ADDED_MESSAGES))
         assert expected_messege == actual_messege, F'"{actual_messege}" не соответсвует ожидаемому результату: "{expected_messege}"'
 
     def message_product_price(self):
-        expectes_messege = self.get_price_product()
+        expectes_messege = self.get_price_product_text()
         actual_message = self.browser.find_element(*ProductPageLocators.MESSAGE_PRODUCT_PRICE).text
         assert expectes_messege == actual_message, F'"{actual_message}" не соответсвует ожидаемому результату: "{expectes_messege}"'
 
